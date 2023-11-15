82781020da82c2386b0b30dd7967ef7d40ae60fd
Snobin <abrahamsnobin2001@gmail.com>
2023-11-15 08:07:55 +0530

    42 commit

--- maximumsubarray(kadanesAlgorithm).py
+++ maximumsubarray(kadanesAlgorithm).py
@@ -4,22 +4,18 @@ import sys
 def maxSubarraySum(arr, n):
     maxi = -sys.maxsize - 1 
     print(maxi)
-
+    sum=0
     for i in range(n):
-        for j in range(i, n):
-         
-            summ = 0
-
-     
-            for k in range(i, j+1):
-                summ += arr[k]
-
-            maxi = max(maxi, summ)
-
+        sum += arr[i]
+        if sum > maxi:
+            maxi = sum
+        if maxi < 0:
+            sum = 0
     return maxi
 
 
-arr = [-2, -3, 4, -1, 0,-2,2, 1, -5, 4]
+#Test Case
+arr = [-2, -3, 4,2, 1, -5]
 n = len(arr)
 maxSum = maxSubarraySum(arr, n)
 print("The maximum subarray sum is:", maxSum)
